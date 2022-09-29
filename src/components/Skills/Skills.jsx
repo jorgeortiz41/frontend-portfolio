607b340e25e8aff8354546588fbfa7ce270a46ef
Jorge A Ortiz <jorge.ortiz41@upr.edu>
2022-09-29 12:24:55 -0400

    moved src files outside of folder

---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Box, Spacer, Flex, Text, Image, Button, ButtonGroup,
+ } from '@chakra-ui/react'
+
+
+
+function Skills() {
+  return (
+    <Box>
+      <Flex justifyContent="center" flex='1' justify='space-between' mx='100' >
+            <Box as='div' alignItems='center' justifyContent='space-between'>
+              niggaroni
+            </Box>
+      </Flex>
+    </Box>
+  )
+}
+
+export default Skills 
